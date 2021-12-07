89ec34c95e3f6f33c7e56a3ab9dfc88d1bc8c19a
Zachary Cutlip <uid000@gmail.com>
2021-12-06 16:15:54 -0800

    bump version to 0.1.2

--- py_dict_repr/__about__.py
+++ py_dict_repr/__about__.py
@@ -1,5 +1,5 @@
 __title__ = "py-dict-repr"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __summary__ = "An abstract class to enable abitrary classes to define their own dictionary representation."
 
 """
