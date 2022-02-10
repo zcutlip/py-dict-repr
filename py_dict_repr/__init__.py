2a248605b769be3c49ea9d4cbf2da49d46d72f1e
Zachary Cutlip <uid000@gmail.com>
2022-02-09 16:10:16 -0800

    update package initializer

--- py_dict_repr/__init__.py
+++ py_dict_repr/__init__.py
@@ -1,13 +1,7 @@
-from .__about__ import (
+from .__about__ import (  # noqa: F401
     __version__,
     __title__,
     __summary__
 )
 
-from .py_dict_repr import DictRepr, KeyTuple  # noqa: F401
-
-__all__ = [
-    "__version__",
-    "__title__",
-    "__summary__"
-]
+from .py_dict_repr import DictRepr, DictWithCycles, KeyTuple  # noqa: F401
